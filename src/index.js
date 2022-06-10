3dbf2810553e582ee0b9efd11bac6f4570f1308e
Bazil Zainal <bazilzainal@gmail.com>
2022-06-10 18:48:43 +0800

    Add .htaccess file and change to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./pages/Login";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
 
-        {/* Using HashRouter to ensure browser history is preserved properly in production */}
-        {/* There are probably better ways but that's for future exploration */}
-        <HashRouter>
+        <BrowserRouter>
             <Login />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
