59d5d66f8dc47eca6a7abc20c8068b60d6aed4eb
Bazil Zainal <bazilzainal@gmail.com>
2022-06-10 17:08:31 +0800

    Change to HashRouter for production

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+
+        {/* Using HashRouter to ensure browser history is preserved properly in production */}
+        {/* There are probably better ways but that's for future exploration */}
+        <HashRouter>
             <Login />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
